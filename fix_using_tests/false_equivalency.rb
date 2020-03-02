5955813c626aa7845cbe1eef6b023d3e7537b80b
Austin Kohler <austinckohler@gmail.com>
2020-03-02 01:29:33 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -13,16 +13,15 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1
+  if num == 1
      "YUM YUM MUNCH MUNCH MUNCH"
     
-  elsif num = 2
+  elsif num == 2
      "HAM HAM HAM IN MY TUMMY"
   end
 end
 
 def runner
   prompt_user
-   binding.pry
   selection(get_user_input)
 end
