43dfdb63c96b9f3bb84d0a9fe95251266fb33676
Austin Kohler <austinckohler@gmail.com>
2020-03-02 01:30:22 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,12 +1,10 @@
 require "pry"
 
 def get_user_input
-
-  selection
+gets.chomp
 end
 
 def prompt_user
-  binding.pry
   puts "What would you like to do?"
   puts "1.) Eat a hamburger."
   puts "2.) Eat a ham."
